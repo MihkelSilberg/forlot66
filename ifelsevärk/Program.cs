526cc2e0ae57ff0922e7d0d3997d8827084b66b3
opilane <mihkel.silberg@gmail.com>
2024-05-03 12:19:17 +0300

    woolo2

---
+++ ifelsevärk/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ifelsevärk
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            int x = 10;
+            int y = 20;
+
+            if (x > y)
+            {
+                Console.WriteLine("x on suurem kui y");
+            }
+            else
+            {
+                if (y > x)
+                {
+                    Console.WriteLine("y on suurem kui x")
+                }
+                else
+                {
+                    Console.WriteLine("x on väiksem kui y")
+                }
+            }
+        }
+    }
+}
