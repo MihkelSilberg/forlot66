6d2f75fb9967720b2b55d100c47114e8c5faee0b
opilane <mihkel.silberg@gmail.com>
2024-05-03 12:04:10 +0300

    woolo

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
